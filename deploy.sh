155ee3680e42e360b4ec5a8408d653b34df316f9
Kristofer Lundgren <kristofer.lundgren@cgi.com>
2025-10-11 01:02:41 +0200

    Deploy ArgoCD app using Helm and Devbox secrets

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,10 @@
 #!/bin/sh -x
 
 # Deploy Helm chart:
-helm upgrade --install my-infra helmchart/ --take-ownership
+helm upgrade \
+  --install my-infra helmchart/ \
+  --take-ownership \
+  --values <(devbox secrets list --format dotenv --show)
 
 # List installed Helm charts:
 helm list -A --all
