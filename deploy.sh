f7782978687d0b807820226f90b8f734bbbbcf33
Kristofer Lundgren <kristofer@famlundgren.org>
2025-10-12 22:22:26 +0200

    build: fix helm template command

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,8 @@
 devbox secrets list --format dotenv | sed 's|=|: |'
 
 echo Deploying Bootstrap Helm chart...
-helm template \
-  my-infra helmchart-bootstrap \
+helm template my-infra \
+  helmchart-bootstrap \
   --take-ownership \
   --values helmchart/values.yaml \
   --values <(devbox secrets list --format dotenv --show | sed 's|=|: |') \
