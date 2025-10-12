09e9db9c5aba6ff6fceda1e7571192488fbe60cb
Kristofer Lundgren <kristofer.lundgren@cgi.com>
2025-10-12 10:45:51 +0200

    Fixed typo in helm template path

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ devbox secrets list --format dotenv | sed 's|=|: |'
 
 # Deploy Helm chart:
 helm template \
-  my-infra helmchart/templpates/bootstrap/ \
+  my-infra helmchart/templates/bootstrap/ \
   --take-ownership \
   --values helmchart/values.yaml \
   --values <(devbox secrets list --format dotenv --show | sed 's|=|: |')
