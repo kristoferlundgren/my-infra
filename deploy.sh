d939147d1a178680605c7d40ed6170b4c42991ab
Kristofer Lundgren <kristofer.lundgren@cgi.com>
2025-10-12 14:03:17 +0200

    build: update bootstrap helm chart deployment

--- deploy.sh
+++ deploy.sh
@@ -3,9 +3,9 @@
 # Show variables:
 devbox secrets list --format dotenv | sed 's|=|: |'
 
-# Deploy Helm chart:
+echo Deploying Bootstrap Helm chart...
 helm template \
-  my-infra helmchart/templates/bootstrap/ \
+  my-infra helmchart-bootstrap \
   --take-ownership \
   --values helmchart/values.yaml \
   --values <(devbox secrets list --format dotenv --show | sed 's|=|: |') \
